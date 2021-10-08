f95cd7156f4172dddcdbfdf3584cd6170c838488
Erick Danzer <edanzer@gmail.com>
2021-10-07 18:45:57 -0600

    Add chart background to orderbook

--- src/components/OrderBook/index.tsx
+++ src/components/OrderBook/index.tsx
@@ -10,9 +10,15 @@ export const OrderBook = () => {
     const { asks, bids } = useSubscribeOrderWorker()
     const [ spread, setSpread ] = useState<number>(0)
     const [ spreadPercent, setSpreadPercent ] = useState<string>('')
+    const [ totalAsks, setTotalAsks ] = useState<number>(0)
+    const [ totalBids, setTotalBids ] = useState<number>(0)
     
     useEffect(() => {
-        if (asks[0] && bids[0]) setSpread(asks[0].price - bids[0].price)
+        if (asks[0] && bids[0]) {
+            setSpread(asks[0].price - bids[0].price)
+            setTotalAsks(asks[asks.length-1].total)
+            setTotalBids(asks[bids.length-1].total)
+        }
     }, [asks, bids])
 
     useEffect(() => {
@@ -28,8 +34,8 @@ export const OrderBook = () => {
                 <div className={styles.title}>Order Book</div>
                 <div className={styles.spread}>Spread: {`${spread} (${spreadPercent})`}</div>
             </div>
-            <OrderBookTable orderType="bid" orders={bids} />
-            <OrderBookTable orderType="ask" orders={asks} />
+            <OrderBookTable orderType="bid" orders={bids} total={totalBids}/>
+            <OrderBookTable orderType="ask" orders={asks} total={totalAsks}/>
             <div className={styles.footer}>
                 <button className={styles.toggle}>Toggle Feed</button>
             </div>
--- src/components/OrderBookTable/index.tsx
+++ src/components/OrderBookTable/index.tsx
@@ -6,10 +6,33 @@ import styles from "./styles.module.css"
 interface OrderBookTableProps {
     orders: FinishedOrder[],
     orderType: OrderType
+    total: number
 }
 
-export const OrderBookTable = ({orders, orderType}: OrderBookTableProps) => {
+interface RowBackground {
+    background: string
+}
 
+export const OrderBookTable = ({orders, orderType, total}: OrderBookTableProps) => {
+
+    const getRowBackground = (rowTotal: number ): RowBackground => {
+        const fraction = rowTotal/total
+        const direction = orderType === "bid" ? "-90deg" : "90deg"
+        const color = orderType === "bid" ? "rgba(0, 163, 108, .15)" : "rgba(238, 75, 43, .15)"
+
+        const background = `linear-gradient(${direction}, ${color} ${toPercent(fraction)}, rgba(0,0,0,0) ${toPercent(fraction + .001)})`
+
+        const rowBackground = {
+            background
+        };
+
+        return rowBackground;
+    }
+
+    const toPercent = (num: number ): string => {
+        return num.toLocaleString('en-US',{style: 'percent', minimumFractionDigits:2})
+    }
+    
     return (
         <div className={styles[orderType + "Table"]}>
             <div className={`${styles.tableHeader} ${styles[orderType]}`}>
@@ -19,7 +42,7 @@ export const OrderBookTable = ({orders, orderType}: OrderBookTableProps) => {
             </div>
                 {
                     orders.map((row: FinishedOrder) => (
-                        <div key={row.price.toFixed(2)} className={`${styles.row} ${styles[orderType]}`}>
+                        <div key={row.price.toFixed(2)} className={`${styles.row} ${styles[orderType]}`} style={getRowBackground(row.total)}>
                             <div className={`${styles.item} ${styles[orderType + "Price"]}`}>{row.price.toFixed(2)}</div>
                             <div className={styles.item}>{row.size.toLocaleString('en')}</div>
                             <div className={styles.item}>{row.total.toLocaleString('en')}</div>
