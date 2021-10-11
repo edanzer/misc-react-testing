2c60d546890495187eb3584e1282026f6bb1cad3
Erick Danzer <edanzer@gmail.com>
2021-10-11 11:38:31 -0600

    Move, rename, purify getRowBackgroundStyles() in OrderBookTable component

--- src/components/OrderBookTable/index.tsx
+++ src/components/OrderBookTable/index.tsx
@@ -13,21 +13,21 @@ interface RowBackground {
     background: string
 }
 
+const getRowBackgroundStyles = (rowTotal: number, total: number, orderType: OrderType ): RowBackground => {
+    const fraction = rowTotal/total
+    const direction = orderType === "bid" ? "-90deg" : "90deg"
+    const color = orderType === "bid" ? "rgba(0, 163, 108, .15)" : "rgba(238, 75, 43, .15)"
+
+    const background = `linear-gradient(${direction}, ${color} ${toPercent(fraction)}, rgba(0,0,0,0) ${toPercent(fraction + .001)})`
+
+    const rowBackground = {
+        background
+    };
+
+    return rowBackground;
+}
+
 export const OrderBookTable = ({orders, orderType, total}: OrderBookTableProps) => {
-
-    const getRowBackground = (rowTotal: number ): RowBackground => {
-        const fraction = rowTotal/total
-        const direction = orderType === "bid" ? "-90deg" : "90deg"
-        const color = orderType === "bid" ? "rgba(0, 163, 108, .15)" : "rgba(238, 75, 43, .15)"
-
-        const background = `linear-gradient(${direction}, ${color} ${toPercent(fraction)}, rgba(0,0,0,0) ${toPercent(fraction + .001)})`
-
-        const rowBackground = {
-            background
-        };
-
-        return rowBackground;
-    }
     
     return (
         <div className={styles[orderType + "Table"]}>
@@ -38,7 +38,7 @@ export const OrderBookTable = ({orders, orderType, total}: OrderBookTableProps) 
             </div>
                 {
                     orders.map((row: FinishedOrder) => (
-                        <div key={row.price.toFixed(2)} className={`${styles.row} ${styles[orderType]}`} style={getRowBackground(row.total)}>
+                        <div key={row.price.toFixed(2)} className={`${styles.row} ${styles[orderType]}`} style={getRowBackgroundStyles(row.total, total, orderType)}>
                             <div className={`${styles.item} ${styles[orderType + "Price"]}`}>{row.price.toFixed(2)}</div>
                             <div className={styles.item}>{row.size.toLocaleString('en')}</div>
                             <div className={styles.item}>{row.total.toLocaleString('en')}</div>
